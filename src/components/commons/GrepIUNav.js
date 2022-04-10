b3202e73226392809ffd20610254b01bcfe989fb
iukim21c <rlawlsan1!>
2022-04-11 01:49:37 +0900

    1. Navbar 배치 수정 2. 스크롤 세로 보이도록 수정

--- src/components/commons/GrepIUNav.js
+++ src/components/commons/GrepIUNav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
 import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
@@ -16,33 +16,36 @@ const GrepIUNav = () => {
     dispatch(logout())
   }
   return <>
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-      <Navbar.Brand as={Link} to="/">GrepIU</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav.Link as={Link} to="/about" className="text-white-50">소개</Nav.Link>
-        <Nav.Link as={Link} to="/post" className="text-white-50">포스팅</Nav.Link>
-        <Nav.Link as={Link} to="/toy" className="text-white-50">토이프로젝트</Nav.Link>
-        <Nav.Link as={Link} to="/cloud" className="text-white-50">클라우드</Nav.Link>
-        {
-          currentUser?.role === 'SUPER_ADMIN'?<Nav.Link as={Link} to="/admin" className="text-white-50">관리자</Nav.Link>:<></>
-        }
-        <Nav.Link as={Link} to="/support" className="text-white-50">지원받기</Nav.Link>
-        <Nav className="me-auto">
-          <NavDropdown title={currentUser ? currentUser.id : '계정'}
-                       id="basic-nav-dropdown">
+    <Navbar bg="dark" variant="dark" expand="lg" sticky>
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/">GrepIU</Navbar.Brand>
+        <Navbar.Toggle/>
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/about" className="text-white-50">소개</Nav.Link>
+            <Nav.Link as={Link} to="/post" className="text-white-50">포스팅</Nav.Link>
+            <Nav.Link as={Link} to="/toy" className="text-white-50">토이프로젝트</Nav.Link>
+            <Nav.Link as={Link} to="/cloud" className="text-white-50">클라우드</Nav.Link>
             {
-              currentUser ?
-                  <NavDropdown.Item onClick={onLogout}>로그아웃</NavDropdown.Item> :
-                  <>
-                    <LinkContainer to="/login">
-                      <NavDropdown.Item>로그인</NavDropdown.Item>
-                    </LinkContainer>
-                  </>
+              currentUser?.role === 'SUPER_ADMIN'?<Nav.Link as={Link} to="/admin" className="text-white-50">관리자</Nav.Link>:<></>
             }
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
+            <Nav.Link as={Link} to="/support" className="text-white-50">지원받기</Nav.Link>
+          </Nav>
+          {
+            currentUser ?
+                <Navbar.Text onClick={onLogout}><div style={{
+                  cursor: 'pointer'
+                }}>로그아웃</div></Navbar.Text> :
+                <>
+                  <LinkContainer to="/login" style={{
+                    cursor: 'pointer'
+                  }}>
+                    <Navbar.Text>로그인</Navbar.Text>
+                  </LinkContainer>
+                </>
+          }
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   </>
 }
