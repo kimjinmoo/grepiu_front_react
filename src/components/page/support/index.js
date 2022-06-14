ec03f3116b11eed74f5e6bcd994b571c6bb107b6
iukim21c <rlawlsan1!>
2022-06-14 19:39:16 +0900

    1. 포스팅 상세보기 스크롤 버그 수정 2. 로또 통계 페이지 추가

--- src/components/page/support/index.js
+++ src/components/page/support/index.js
@@ -1,41 +1,59 @@
 import React, {useState} from 'react';
 import {Button, Form} from "react-bootstrap";
 import ReactQuill from "react-quill";
-import {
-  formats,
-  supportModules
-} from "../../../constancs/quill_options";
+import {formats, modules} from "../../../constancs/quill_options";
+import {addSupportReport} from "../../../services/firebase.service";
+import {NotificationManager} from "react-notifications";
 
 const Support = () => {
 
   // 제목
-  const [subject, setSubject] = useState();
+  const [subject, setSubject] = useState('');
   // 내용
-  const [content, setContent] = useState();
+  const [contents, setContents] = useState('');
+  // 타입
+  const [type, setType] = useState("1");
+
+  // 접속
+  const submit = async () => {
+    await addSupportReport(subject, contents, type);
+    setType("1")
+    setContents('');
+    setSubject('');
+    NotificationManager.success('접속 완료 되었습니다.');
+  }
 
   return <div>
-    <Form.Select aria-label="선택하여 주세요">
+    <Form.Select value={type} aria-label="선택하여 주세요"
+                 onChange={e => setType(e.target.value)}
+    >
       <option value="1">홈페이지 문의</option>
       <option value="2">Grep Search App 문의</option>
       <option value="3">우리어때 문의</option>
+      <option value="3">한방!로또 문의</option>
     </Form.Select>
     <Form.Control
         type="text"
         id="subject"
         aria-describedby="subjectBlock"
         placeholder="제목"
+        onChange={e => setSubject(e.target.value)}
         value={subject}
     />
     <Form.Text id="subject" muted>
       제목을 입력하세요
     </Form.Text>
     <ReactQuill
-        theme="snow" value={content} onChange={setContent}
-        modules={supportModules}
+        theme="snow" value={contents}
+        onChange={(content, delta, source, editor) => {
+          setContents(content);
+        }
+        }
+        modules={modules}
         formats={formats}
     />
     <div className="d-flex align-items-end">
-      <Button className="ms-auto m-2">문의하기</Button>
+      <Button className="ms-auto m-2" onClick={submit}>문의하기</Button>
     </div>
   </div>
 };
--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -1,4 +1,4 @@
-import {collection, getDocs, getFirestore, query, where} from "firebase/firestore/lite";
+import {collection, addDoc, getDocs, getFirestore, query, where} from "firebase/firestore/lite";
 import {lottoFirebase} from "../constancs/firebase";
 
 
@@ -17,3 +17,26 @@ export const fetchLottoNumberByRound = async  (round) => {
   const obj = lottoSnapshot.docs.map(doc => doc.data());
   return obj;
 }
+
+/**
+ *
+ * 문의하기
+ *
+ * @param subject 제목
+ * @param contents 내용
+ * @param type 타입
+ * @returns {Promise<void>}
+ */
+export const addSupportReport = async (subject, contents, type) => {
+  try {
+    const docRef = await addDoc(collection(lottoDB, "help-service"), {
+      subject: subject,
+      contents: contents,
+      type: type
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
